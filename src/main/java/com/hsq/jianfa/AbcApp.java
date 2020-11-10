3d5a11293e0fda8c97afbb067f8ac573db7c5f30
hsq <huangshenqing1@live.com>
2020-11-10 10:33:39 +0800

    剑法升级

---
+++ src/main/java/com/hsq/jianfa/AbcApp.java
@@ -0,0 +1,4 @@
+package com.hsq.jianfa;
+
+public class AbcApp {
+}
--- src/main/java/com/hsq/jianfa/Dugujiujian1201.java
+++ src/main/java/com/hsq/jianfa/Dugujiujian1201.java
@@ -3,5 +3,6 @@ package com.hsq.jianfa;
 public class Dugujiujian1201 {
     public static void main(String[] args) {
         System.out.println("hello dugujiujian1");
+        System.out.println("hello ");
     }
 }
